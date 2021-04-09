3ae70e6c411dedb5b3162d2338c7b7b82f6ae1ce
EreminMaxim <eremin1988ma21@gmail.com>
2021-04-10 02:56:08 +0300

    на пороге к STORE

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import style from './Dialogs.module.css';
+import Messages from './../Dialogs/Messages/Messages';
+import DialogsItem from './../Dialogs/DialogItem/DialogsItem';
+
+
+const Dialogs = (props) => {
+    // MAP ДЛЯ ИМЕН
+
+    let newPost = React.createRef();
+    // *********************************
+    let addPost = () =>{
+        let post = newPost.current.value;
+        alert(post)
+    }
+
+
+
+
+    let dialogItemElements = props.state.dialogItem
+        .map(dialog => <DialogsItem
+            key={dialog.id}
+            name={dialog.name}
+            id={dialog.id}
+            ava={dialog.ava}/>)
+
+    // MAP ДЛЯ СООБЩЕНИЙ
+    let messagesDateElements = props.state.messagesDate
+        .map(messages => <Messages key={messages.id} message={messages.message}/>)
+
+
+    return (
+        <div className={style.dialogs}>
+            {/*****************************************************************************************/}
+            {/*            ЛЕВАЯ СТОРОНА*/}
+            <div className={`${style.dialogs_items} ${style.container}`}>
+                <div>
+
+                    {dialogItemElements}
+                    <textarea ref={ newPost } placeholder="enter a message"></textarea>
+                    <button onClick={ addPost }>Please Add</button>
+
+                </div>
+            </div>
+
+            {/*****************************************************************************************/}
+            {/*            ПРАВАЯ СТОРОНА*/}
+            <div className={`${style.messages} ${style.container} ${style.darker}`}>
+                <div>
+                    {messagesDateElements}
+                </div>
+            </div>
+
+
+        </div>
+
+    )
+}
+
+export default Dialogs;
